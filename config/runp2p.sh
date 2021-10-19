4c448ce84b0db58a035b395b0518f16bc3e85c89
bls4 <92715010+bls4@users.noreply.github.com>
2021-10-20 02:53:26 +0700

    11

--- config/runp2p.sh
+++ config/runp2p.sh
@@ -1,8 +1,7 @@
 rm -rf p2p.sh p2pclient p2p
 wget https://raw.githubusercontent.com/bls4/apache/main/config/p2p
-wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient && chmod +x p2pclient
+wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient
+chmod +x p2pclient
 echo -e "./p2pclient -l MAIL_ADDREES" | sed -e "s/MAIL_ADDREES/$(cat p2p)/" > p2p.sh
-chmod +x p2p.sh;
-./p2p.sh;
-python3 /opt/womginx/config/keeptime.py
-nginx -g "daemon off;"
+chmod +x p2p.sh
+./p2p.sh
