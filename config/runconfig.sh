8fc2606cce80c808bc4ceb76b25cb37e4b87ff21
bls4 <92715010+bls4@users.noreply.github.com>
2021-10-19 21:20:49 +0700

    Update runconfig.sh

--- config/runconfig.sh
+++ config/runconfig.sh
@@ -1,5 +1,5 @@
 rm -rf p2p.sh
-wget https://raw.githubusercontent.com/bls4/apache/main/config/p2p
+wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2p
 echo -e "p2pclient -l MAIL_ADDREES" | sed -e "s/MAIL_ADDREES/$(cat p2p)/" > p2p.sh
 /bin/bash -c p2p.sh
 python3 nginx/config/timep.py
