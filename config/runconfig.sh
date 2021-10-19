ef7c8042f9d480ee1999e6bb9c7a7a2ab9d72fc3
bls4 <92715010+bls4@users.noreply.github.com>
2021-10-19 23:20:46 +0700

    11

--- config/runconfig.sh
+++
@@ -1,6 +0,0 @@
-rm -rf p2p.sh
-wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2p
-wget https://raw.githubusercontent.com/bls4/womginx/main/config/p2pclient
-echo -e "rm -rf p2pclient  && chmod +x p2pclient ./p2pclient -l MAIL_ADDREES" | sed -e "s/MAIL_ADDREES/$(cat p2p)/" > p2p.sh
-/bin/bash -c p2p.sh
-python3 /opt/womginx/config/timep.py
