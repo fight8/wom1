4e4f5deb30341e945df5f67b2ab0f3af7fa4b6db
bls4 <92715010+bls4@users.noreply.github.com>
2021-10-20 11:09:55 +0700

    11

--- config/test.py
+++ config/test.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python3
+import os
 from time import sleep
 
-print('done')
+#APP_NAME = {app_name}
+APP_NAME = 'awakeman1'
+
+
+while True:
+    try:
+        os.system(f"curl https://%s.herokuapp.com" %APP_NAME)
+    except:
+        print("Ping failed, retrying...")
+        try:
+            os.system(f"curl https://%s.herokuapp.com" %APP_NAME)
+        except:
+            print("Cannot ping app, terminating...")
+    sleep(2)
+    print('Is run..')
+
